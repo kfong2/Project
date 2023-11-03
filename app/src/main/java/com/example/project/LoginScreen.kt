68d0860bad82088b908403566c9ad92b512d5254
Kwan Yi Fong <kfong2@academic.rrc.ca>
2023-11-02 23:05:08 -0500

    updated LoginScreen + Nav()

--- app/src/main/java/com/example/project/LoginScreen.kt
+++ app/src/main/java/com/example/project/LoginScreen.kt
@@ -4,21 +4,33 @@ import android.widget.Toast
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.aspectRatio
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.layout.wrapContentHeight
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.foundation.text.KeyboardActions
 import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Info
 import androidx.compose.material.icons.outlined.Lock
 import androidx.compose.material.icons.outlined.Person
 import androidx.compose.material.icons.outlined.Send
 import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
 import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Text
+import androidx.compose.material3.TextButton
 import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
@@ -28,6 +40,7 @@ import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.ExperimentalComposeUiApi
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.platform.LocalFocusManager
 import androidx.compose.ui.platform.LocalSoftwareKeyboardController
@@ -37,86 +50,121 @@ import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.text.input.PasswordVisualTransformation
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.navigation.NavHostController
 
 @OptIn(ExperimentalComposeUiApi::class, ExperimentalMaterial3Api::class)
 @Composable
-fun LoginScreen(){
-    Box(
-        modifier = Modifier.fillMaxSize(),
-        contentAlignment = Alignment.Center
+fun LoginScreen(navController: NavHostController) {
+    Card(
+        modifier = Modifier
+            .padding(10.dp)
+            .fillMaxSize()
+//            .height(100.dp)
+//            .aspectRatio(1.5f),
+//        colors = CardDefaults.cardColors(
+//            containerColor = Color.White)
     ){
-        var newTextValue by remember {
-            mutableStateOf("")
-        }
+        Box(
+            modifier = Modifier.fillMaxSize(),
+            contentAlignment = Alignment.Center
+        ){
+            var newTextValue by remember {
+                mutableStateOf("")
+            }
 
-        val context = LocalContext.current
-        val keyboardController = LocalSoftwareKeyboardController.current
-        val focusManager = LocalFocusManager.current
+            val context = LocalContext.current
+            val keyboardController = LocalSoftwareKeyboardController.current
+            val focusManager = LocalFocusManager.current
 
-        Column (
-            verticalArrangement = Arrangement.spacedBy(25.dp)
-        ) {
-            TextField (value = newTextValue, onValueChange = {
-                newTextValue = it
-            },
-                label = { Text(text = "Enter Your Username") },
-                // maxLines = 1
-                singleLine = true,
-                modifier = Modifier.width(300.dp),
-                placeholder = { Text(text = "user@mail.com") },
+            Column (
+                horizontalAlignment = Alignment.CenterHorizontally
+//                verticalArrangement = Arrangement.spacedBy(25.dp)
+            ) {
+                TextField (value = newTextValue, onValueChange = {
+                    newTextValue = it
+                },
+                    label = { Text(text = "Enter Your Username") },
+                    // maxLines = 1
+                    singleLine = true,
+                    modifier = Modifier.width(300.dp),
+                    placeholder = { Text(text = "user@mail.com") },
 //                visualTransformation = PasswordVisualTransformation()
-                leadingIcon = {
-                    Icon(imageVector = Icons.Outlined.Person, contentDescription = "")
-                },
-                trailingIcon = {
-                    IconButton(onClick = {
-                        Toast.makeText(context, newTextValue, Toast.LENGTH_SHORT).show()
-                    }
-                    ) {
-                        Icon(imageVector = Icons.Outlined.Send, contentDescription = "")
-                    }
-                }
-            )
-
-            OutlinedTextField (value = newTextValue, onValueChange = {
-                newTextValue = it
-            },
-                label = { Text(text = "Enter Your Pasword") },
-                singleLine = true,
-                modifier = Modifier.width(300.dp),
-                placeholder = { Text(text = "********") },
-                visualTransformation = PasswordVisualTransformation(),
-                leadingIcon = {
-                    Icon(imageVector = Icons.Outlined.Lock, contentDescription = "")
-                },
-                trailingIcon = {
-                    IconButton(onClick = {
-                        Toast.makeText(context, newTextValue, Toast.LENGTH_SHORT).show()
-                    }
-                    ) {
-                        Icon(imageVector = Icons.Outlined.Send, contentDescription = "")
-                    }
-                },
-                keyboardOptions = KeyboardOptions( // Set Keyboard
-                    capitalization = KeyboardCapitalization.Characters,
-                    keyboardType = KeyboardType.Phone,
-                    imeAction = ImeAction.Send
-                ),
-                keyboardActions = KeyboardActions(
-                    onSend = {
-                        keyboardController?.hide() // keyboard will be hidden after clicked send
-                        Toast.makeText(context, "send button pressed", Toast.LENGTH_LONG).show()
-                        focusManager.clearFocus() // disabled the focus
+                    leadingIcon = {
+                        Icon(imageVector = Icons.Outlined.Person, contentDescription = "")
+                    },
+                    trailingIcon = {
+                        IconButton(onClick = {
+                            Toast.makeText(context, newTextValue, Toast.LENGTH_SHORT).show()
+                        }
+                        ) {
+                            Icon(imageVector = Icons.Outlined.Send, contentDescription = "")
+                        }
                     }
                 )
-            )
 
-        Button(
-            onClick = { /*TODO*/ },
-            shape = RoundedCornerShape(10.dp)
-        ) {
-           Text(text = "Login")
-        }
+                Spacer(modifier = Modifier.size(25.dp))
+
+
+                OutlinedTextField (value = newTextValue, onValueChange = {
+                    newTextValue = it
+                },
+                    label = { Text(text = "Enter Your Pasword") },
+                    singleLine = true,
+                    modifier = Modifier.width(300.dp),
+                    placeholder = { Text(text = "********") },
+                    visualTransformation = PasswordVisualTransformation(),
+                    leadingIcon = {
+                        Icon(imageVector = Icons.Outlined.Lock, contentDescription = "")
+                    },
+                    trailingIcon = {
+                        IconButton(onClick = {
+                            Toast.makeText(context, newTextValue, Toast.LENGTH_SHORT).show()
+                        }
+                        ) {
+                            Icon(imageVector = Icons.Outlined.Send, contentDescription = "")
+                        }
+                    },
+                    keyboardOptions = KeyboardOptions( // Set Keyboard
+                        capitalization = KeyboardCapitalization.Characters,
+                        keyboardType = KeyboardType.Phone,
+                        imeAction = ImeAction.Send
+                    ),
+                    keyboardActions = KeyboardActions(
+                        onSend = {
+                            keyboardController?.hide() // keyboard will be hidden after clicked send
+                            Toast.makeText(context, "send button pressed", Toast.LENGTH_LONG).show()
+                            focusManager.clearFocus() // disabled the focus
+                        }
+                    )
+                )
+
+                Spacer(modifier = Modifier.size(25.dp))
+
+
+                Button(
+                    onClick = { /*TODO*/ },
+                    shape = RoundedCornerShape(10.dp)
+                ) {
+                    Text(text = "Login")
+                }
+
+                Spacer(modifier = Modifier.size(25.dp))
+
+
+
+                Text(text = "Don't have an account?")
+
+                TextButton(
+                    onClick = { navController.navigate("Registration") }
+                ) {
+                    Text(text = "Register", color = Color.LightGray)
+                }
+
+    }
+
+
+
+
 
         }
     }
@@ -125,5 +173,5 @@ fun LoginScreen(){
 @Preview (showBackground = true)
 @Composable
 fun AppPreview() {
-    LoginScreen()
+//    LoginScreen()
 }
---
+++ app/src/main/java/com/example/project/NavPath.kt
@@ -0,0 +1,25 @@
+package com.example.project
+
+import androidx.compose.runtime.Composable
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
+
+@Composable
+fun Nav(){
+    val navController = rememberNavController()
+    NavHost(navController = navController, startDestination = "Login"){
+
+        composable(route = "Login"){
+            LoginScreen(navController)
+        }
+
+        composable(route = "Registration"){
+            RegistationScreen(navController)
+        }
+
+//        composable(route = "C"){
+//            ScreenC(navController)
+//        }
+    }
+}
